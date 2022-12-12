5eab60e5248f15bec4efdc25c2025f729942c16f
yanganthony4 <yanganthony4@gmail.com>
2022-12-12 15:31:05 -0700

    added initial doctor menu and fixed patient.py

--- doctor.py
+++ doctor.py
@@ -1,12 +1,36 @@
 class formatDrInfo:
-    def __init__ (self, id, name, specialty, schedule, qualification, room_number):
+    def __init__ (self, id, name, speciality, schedule, qualification, room_number):
         self.id = id
         self.name = name
         self.speciality = speciality
         self.schedule = schedule
         self.qualification = qualification
         self.room_number = room_number
-    
+
+def doctorsMenu():
+    print("Doctor's Menu")
+    print("0 - Return to Main Menu\n1 - Display Doctors list\n2 - Search for doctor by ID\n3 - Search for doctor by name\n4 - Add doctor\n5 - Edit doctor info")
+    option = 1
+    doctors_list = readDoctorsFile()
+    while option != 0:
+        option = int(input("Enter option: "))
+        if option == 1:
+            displayDoctorsList(doctors_list)
+        elif option == 2:
+            searchDoctorById(doctors_list)
+        elif option == 3:
+            searchDoctorByName(doctors_list)
+            
+        elif option == 4:
+            doctor = enterDrInfo()
+            addDrToList(doctor,doctors_list)
+        elif option == 5:
+            editDoctorInfo(doctors_list)
+            displayDoctorsList(doctors_list)
+
+
+
+        
 def enterDrInfo():
     id = input("Enter Dr ID: ")
     name = input("Enter Dr name: ")
@@ -23,7 +47,60 @@ def readDoctorsFile():
         for line in file:
             if not line.isspace():
                 line = line.replace("\n","").split("_")
-                doctor = formatDrInfo(line)
+                doctor = formatDrInfo(line[0], line[1], line[2], line[3], line[4], line[5])
                 doctors_list.append(doctor)
+    return doctors_list
 
-readDoctorsFile()
+def searchDoctorById(doctors_list):
+    search_id = int(input("Enter the doctor ID: "))
+    for doctor in doctors_list:
+        if doctor.id == search_id:
+            return doctor
+    return "Doctor with ID " + search_id + " not in doctor file"
+
+def searchDoctorByName(doctors_list):
+    search_name = input("Enter the doctor name: ")
+    for doctor in doctors_list:
+        if doctor.name == search_name:
+            return doctor
+    return "Doctor with name " + search_name + " not in doctor file"
+
+def editDoctorInfo(doctors_list):
+    doctor = searchDoctorById(doctors_list)
+    print(doctor)
+
+    doctor.name = input("Enter new name: ")
+    doctor.speciality = input("Enter new speciality in: ")
+    doctor.schedule = input("Enter new schedule: ")
+    doctor.qualification = input("Enter new qualifications: ")
+    doctor.room_number = input("Enter new room number: ")
+    return doctor
+
+def displayDoctorsList(doctors_list):
+    print("\n")
+    for doctor in doctors_list:
+        print(doctor)
+    
+def writeDoctorsListToFile(doctors_list):
+    f = open("data\doctors.txt")
+    f.truncate(0)
+
+    for doctor in doctors_list:
+        doctors_write_list = []
+        doctors_write_list.append(doctor.id)
+        doctors_write_list.append(doctor.name)
+        doctors_write_list.append(doctor.speciality)
+        doctors_write_list.append(doctor.schedule)
+        doctors_write_list.append(doctor.qualifications)
+        doctors_write_list.append(doctor.room_number)
+        f.write("_".join(doctors_write_list))
+
+def addDrToList(doctor, doctors_list):
+    doctors_list.append(doctor)
+    return(doctors_list)
+
+option = 1
+doctors_list = readDoctorsFile()
+
+while option != 0:
+    option = doctorsMenu
